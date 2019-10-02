7142edac4608edf7f217cd691a03e4213b8b8623
Kevin Krato <kratok08@gmail.com>
2019-10-02 12:42:23 -0600

    Test it returns the correct values when checked against categories

--- test/round_test.rb
+++ test/round_test.rb
@@ -3,16 +3,17 @@ require 'minitest/pride'
 require "../lib/card"
 require '../lib/deck'
 require '../lib/round'
+require "../lib/turn"
 
 class RoundTest < Minitest::Test
   def setup
-    card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-    card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-    card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
+    @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
+    @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
+    @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
 
-    cards = [card_1, card_2, card_3]
+    @cards = [@card_1, @card_2, @card_3]
 
-    @deck = Deck.new(cards)
+    @deck = Deck.new(@cards)
     @round = Round.new(@deck)
   end
 
@@ -23,4 +24,11 @@ class RoundTest < Minitest::Test
   def test_it_should_have_a_deck_on_initialize
     assert_equal @deck, @round.deck
   end
+
+  def test_it_should_return_number_correct_by_category
+    @round.take_turn("Juneau")
+    assert_equal 1, @round.number_correct_by_category(:Geography)
+    assert_equal 0, @round.number_correct_by_category(:STEM)
+  end
+
 end
